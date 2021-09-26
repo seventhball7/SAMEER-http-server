5c2e30f6efe21454d876d90e9c677be4603c5ddb
Sameer Srivastava <sameer.srivastava.17@mountblue.tech>
2021-09-26 22:18:15 +0530

    using express

--- file.js
+++ file.js
@@ -1,77 +1,48 @@
-const http = require("http");
-const code = require("http").STATUS_CODES;
-const fs = require("fs");
+const express = require("express");
+const app = express();
 const path = require("path");
+const code = require("http").STATUS_CODES;
 
-//console.log(code)
+const port = 3000;
 
 const uuid = {
   uuid: "14d96bb1-5d53-472f-a96e-b3a1fa82addd",
 };
 
-let filePath;
+app.get("/", (req, res) => {
+  res.status(200);
+  res.send("<h1>This is landing page</h1>");
+});
 
-const server = http.createServer((req, res) => {
-  let arrayOfUrl = req.url.split("/");
-  let statusCode = arrayOfUrl[arrayOfUrl.length - 1];
+app.get("/html", (req, res) => {
+  res.status(200).sendFile(path.join(__dirname, "/data/data.html"));
+});
 
-  if (req.url == "/") {
-    res.write("Welcome here");
-    res.end();
-  } else if (req.url == "/html") {
-    filePath = path.join(__dirname, "/data/data.html");
-    fs.readFile(filePath, "utf8", (err, data) => {
-      if (err) {
-        res.writehead(501, "Not implemented");
-        res.write("Unable to read html file");
-        res.end();
-      } else {
-        res.write(data);
-        res.end();
-        console.log(req.url);
-      }
-    });
-  } else if (req.url == "/json") {
-    filePath = path.join(__dirname, "/data/data.json");
-    fs.readFile(filePath, "utf-8", (err, data) => {
-      if (err) {
-        res.writehead(501, "Not implemented");
-        res.write("unable to read json file");
-        res.end();
-      } else {
-        res.write(data);
-        res.end();
-        console.log(req.url);
-      }
-    });
-  } else if (req.url == "/uuid") {
-    res.write(JSON.stringify(uuid));
-    res.end();
-    console.log(req.url);
-  } else if (req.url == `/status/${statusCode}`) {
-    if (code[statusCode] != undefined) {
-      res.writeHead(200, "OK", { "Content-type": "text/html" });
-      res.write(`<h1>${statusCode} --> ${code[statusCode]}</h1>`);
-      res.end();
-      console.log(statusCode);
-    } else {
-      res.writeHead(501, "Not implemented", { "Content-type": "text/html" });
-      res.write("<h1>Not able to find the status code</h1>");
-      res.end();
-    }
-  } else if (req.url == `/delay/${statusCode}`) {
-    setTimeout(() => {
-      res.writeHead(200, "OK", { "Content-type": "text/html" });
-      res.write("<h1>SUCCESS</h1>");
-      res.end();
-    }, statusCode);
+app.get("/json", (req, res) => {
+  res.status(200);
+  res.sendFile(path.join(__dirname, "/data/data.json"));
+});
+
+app.get("/uuid", (req, res) => {
+  res.json(uuid);
+});
+
+app.get("/status/:statusCode", (req, res) => {
+  const statusCode = req.params.statusCode;
+  if (statusCode in code) {
+    res.json({ statusCode: code[statusCode] });
   } else {
-    res.writeHead(404, "Not Found", { "Content-type": "text/html" });
-    res.write("<h1>Not found the page</h1>");
-    res.end();
+    res.status(404).send("<h1>File not found<h1>");
   }
 });
 
-server.listen(8080, () => {
-  console.log(`listening from port 8080`);
+app.get("/delay/:time", (req, res) => {
+  let delayTime = req.params.time;
+  setTimeout(() => {
+    res.send(`<h1>success after ${delayTime} seconds</h1>`);
+  }, delayTime * 1000);
+});
+
+app.listen(port, () => {
+  console.log(`listening to port ${port}`);
 });
